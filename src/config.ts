93958fab58b8daedfe779189808b718ecd49c39c
arthur-popov <arthur.popov@1507.io>
2023-10-29 22:38:30 +0100

    2

--- src/config.ts
+++ src/config.ts
@@ -9,14 +9,14 @@ export const rpcHost =
 
 export const candyMachineId = new PublicKey(
   process.env.NEXT_PUBLIC_CANDY_MACHINE_ID ||
-    "3zwFR3spiwbSSMtvVKG2bRT6ttqFoC3MHCafGP8ZrdLz"
+    "G7oNorqF73EpuTZKLzuS87bvHsoyRNzfKCmk3N5VGkc8"
 );
 export const defaultGuardGroup =
   process.env.NEXT_PUBLIC_DEFAULT_GUARD_GROUP || undefined; // undefined means default
 
-// "qasJ6jhgtngKk2QnEPdDjuFH8NMoM58W8TxPBXAChPY"
-// "3zwFR3spiwbSSMtvVKG2bRT6ttqFoC3MHCafGP8ZrdLz"
-// "DAA8yRLu7acVs3kxaTyCjoEjNWGinLaCKVhDY29ASNua"
+// "8qZZYsLxMBE4vBbWzz7FXFbR7NrMGbEhgGvhVxsPZ4xf"
+// "4i36qPmC4B4AyjhRV1eSfaCw3B7jcUSfRgXcEu8wxWQ5"
+// "2mWemAEQW131oKvcNzYTR7EpeK26ugRQvmCW5U5oLg6a"
 
 export const whitelistedWallets = [
   "53VVFtLzzi3nL2p1QF591PAB8rbcbsirYepwUphtHU9Q",
